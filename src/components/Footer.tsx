4c5056c89886e8c1d2d7f9720a8321dae2d4ca07
PauloMisaki <ph.sorata@gmail.com>
2020-09-15 02:52:01 -0300

    Finalização do Modelo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,12 @@
 export function Footer() {
   return (
-    <div>
-      <div></div>
-      <div></div>
+    <div className="flex flex-col pt-10 pb-8 bg-black">
+      <p className="flex justify-center text-sm text-white">
+        © 2020 Creare Arquitetura, Avenida Pasteur 520, Rio de Janeiro 04719-001
+      </p>
+      <p className="flex justify-center text-sm text-white">
+        Desenvolvido por Paulo Henrique
+      </p>
     </div>
   )
 }
